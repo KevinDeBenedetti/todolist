26b4a84aebd6e93c11072d1daf41c41252433ed9
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-16 12:23:09 +0200

    Fix CORS & Update Swagger

--- backend/todolist/swagger.py
+++ backend/todolist/swagger.py
@@ -4,12 +4,25 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="API Todo List",
+        title="Todo List API",
         default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@kevindb.dev"),
-        license=openapi.License(name="BSD License"),
+        description=(
+            "L'API de gestion de tâches permet de gérer les tâches quotidiennes, "
+            "de les organiser et de les suivre efficacement. Vous pouvez créer, "
+            "modifier, supprimer des tâches et suivre leur état de complétion. "
+            "L'API est sécurisée via des tokens statiques et permet des accès "
+            "publics limités."
+        ),
+        terms_of_service="https://todolist.kevindb.dev/terms/",
+        contact=openapi.Contact(
+            name="Kevin De Benedetti",
+            email="contact@kevindb.dev",
+            url="https://kevindb.dev"
+        ),
+        license=openapi.License(
+            name="MIT License",
+            url="https://opensource.org/licenses/MIT"
+        ),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
