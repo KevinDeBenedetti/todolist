05f530304b1342ea1b217e02c1b32c35c01484f9
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-16 12:48:45 +0200

    Update settings.py

--- backend/task_manager/settings.py
+++ backend/task_manager/settings.py
@@ -144,6 +144,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+    'OPTIONS',
+]
+
 CORS_ALLOW_HEADERS = [
     'authorization',
     'content-type',
