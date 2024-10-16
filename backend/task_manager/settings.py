53c6293aed4afffe45afbe7fccac740c61081a2b
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-16 11:29:33 +0200

    add CORS

--- backend/task_manager/settings.py
+++ backend/task_manager/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todolist',
+    'corsheaders',
     'rest_framework',
     'drf_yasg',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -138,6 +140,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS
+CORS_ALLOW_ALL_ORIGINS = True
 
 # API REST
 REST_FRAMEWORK = {
